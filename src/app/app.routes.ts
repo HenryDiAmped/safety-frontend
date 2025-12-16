5d4612fa7532757e5bf7f2fc697345a65d6ac145
HenryDiAmped <enciso26.2004@gmail.com>
2025-12-15 22:12:52 -0500

    Hice el ruteado usadon RouterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './funcionalidades/home/inicio';
+import { CursoLista } from './funcionalidades/cursos/curso-lista';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
 
   { path: 'inicio', component: InicioComponent },
+
+  { path: 'cursos', component: CursoLista },
 
   { path: '**', redirectTo: 'inicio' },
 ];
