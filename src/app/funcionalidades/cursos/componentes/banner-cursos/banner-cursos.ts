cf699bd56e7e336bb76bc856644864363b774353
HenryDiAmped <enciso26.2004@gmail.com>
2025-12-15 22:13:59 -0500

    Implementacion de componentes de cursos

---
+++ src/app/funcionalidades/cursos/componentes/banner-cursos/banner-cursos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-cursos',
+  standalone: true,
+  templateUrl: './banner-cursos.html',
+  styleUrl: './banner-cursos.css'
+})
+export class BannerCursosComponent {}
